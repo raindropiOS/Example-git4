d1b80f0bc8e372e7141fb7368cc95463d94ed564
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2022-12-03 22:06:22 +0900

    init proj

---
+++ Example-git4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Example-git4
+//
+//  Created by 박시현 on 2022/12/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example-git4/Example_git4App.swift
@@ -0,0 +1,17 @@
+//
+//  Example_git4App.swift
+//  Example-git4
+//
+//  Created by 박시현 on 2022/12/03.
+//
+
+import SwiftUI
+
+@main
+struct Example_git4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
