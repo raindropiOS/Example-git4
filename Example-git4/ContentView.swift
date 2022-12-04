bf31ee9a6e4de61442a87b122a8f3d617946e702
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2022-12-04 21:07:31 +0900

    Revert "commit - 3"
    This reverts commit f45a170a19699091f4f59ce54a7d62f19273ee08.

--- Example-git4/ContentView.swift
+++ Example-git4/ContentView.swift
@@ -18,7 +18,6 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("commit - 1")
             Text("commit - 2")
-            Text("commit - 3")
         }
         .padding()
     }
